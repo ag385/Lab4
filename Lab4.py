07de58a8631aece7a21bc9465c061abf4b005de9
Umbasa <umbasa@Umbasas-MacBook-Air.local>
2021-04-12 11:12:24 -0700

    finished lab 4

--- Lab4.py
+++ Lab4.py
@@ -36,5 +36,23 @@ ask()
 main
 """
 
+def main():
+    question = "What is the capital of California? "
+    answer = "Sacramento"
+    ask(question, answer)
+
+def ask(question, answer, max_tries=3):
+    num_tries = 0
+    while num_tries < max_tries:
+        num_tries += 1
+        ans = input(question)
+        if ans == answer:
+            print("Correct!")
+            break
+    if ans != answer:
+        print("You have used up your allotment of guesses.")
+        print("The correct answer is 'Sacramento'.")
+
+main()
 
 
